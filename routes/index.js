bc16b16aeabfb9b74abe21b730ced798b9ab8f49
Inndy <inndy.tw@gmail.com>
2016-06-01 20:43:20 +0800

    Add login form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+	res.render('login_form', { title: '登入管理'});
+});
+
+router.post('/login', function(req, res, next) {
+	if(req.body.user == 'admin' && req.body.pass == 'admin') {
+		res.send('你登入成功！');
+	} else {
+		res.send('你登入失敗...');
+	}
+});
+
 module.exports = router;
