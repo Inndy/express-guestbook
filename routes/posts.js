b1715a091ce08cbcbb13956b7184ebb009adcea5
Inndy <inndy.tw@gmail.com>
2016-06-01 20:22:44 +0800

    Add delete function

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ var models = require('../models');
 
 // list posts
 router.get('/', function(req, res, next) {
-    models.post.findAll().then(function(posts) {
+    models.Post.findAll().then(function(posts) {
         res.render('posts', { title: '留言列表', posts: posts });
     });
 });
@@ -14,8 +14,19 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/new', function(req, res, next) {
-    models.post.create(req.body);
+    models.Post.create(req.body);
     res.redirect('/posts');
+});
+
+router.get('/delete/:id', function(req, res, next) {
+	var post_id = req.params.id;
+	models.Post.destroy({
+		where: {
+			id: post_id
+		}
+	}).then(function() {
+		res.redirect('/posts');
+	});
 });
 
 module.exports = router;
